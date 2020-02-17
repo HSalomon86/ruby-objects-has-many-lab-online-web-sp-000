584315decd3a067557e9e069910df6c687094957
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-17 15:17:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,6 +15,13 @@ class Author
    @@post_count += 1
  end
 
+ def add_post_by_title(title)
+  post = Post.new(title)
+  @posts << post
+  post.author = self
+  @@post_count += 1
+end
+
   def posts
     @posts = Post.all
   end
