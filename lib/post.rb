1e68d57abfa6c6c1d7e16b3f7d44e8e790926b77
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-17 15:16:53 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,12 @@
 class Post
+
   attr_accessor :title, :author
+
+  @@all = []
+
   def initialize(title)
     @title = title
+    save
   end
 
   def author_name
@@ -10,6 +15,13 @@ class Post
     else
       @author.name
     end
+  end
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
